d30594bb01bf6a0762e26eb018a077084b1a4f1f
Shane Farmer <shane.farmer@gmail.com>
2024-12-04 10:02:41 +1100

    wip: add dampening step filter

--- src/day1.rs
+++ src/day1.rs
@@ -1,89 +1,87 @@
 use std::collections::HashMap;
 
 pub fn day1a(lines: &Vec<String>) -> i32 {
-    let to_int = |s: &str| s.trim().parse::<i32>().unwrap();
+  let to_int = |s: &str| s.trim().parse::<i32>().unwrap();
 
-    let mut ls: Vec<i32> = vec![];
-    let mut rs: Vec<i32> = vec![];
+  let mut ls: Vec<i32> = vec![];
+  let mut rs: Vec<i32> = vec![];
 
-    for line in lines {
-        if let Some((l, r)) = line.split_once(' ').map(|(x, y)| (to_int(x), to_int(y))) {
-            ls.push(l);
-            rs.push(r);
-        }
+  for line in lines {
+    if let Some((l, r)) = line.split_once(' ').map(|(x, y)| (to_int(x), to_int(y))) {
+      ls.push(l);
+      rs.push(r);
     }
+  }
 
-    ls.sort();
-    rs.sort();
+  ls.sort();
+  rs.sort();
 
-    let mut sum = 0;
-    for i in 0..ls.len() {
-        sum += (ls[i] - rs[i]).abs();
-    }
-    sum
+  let mut sum = 0;
+  for i in 0..ls.len() {
+    sum += (ls[i] - rs[i]).abs();
+  }
+  sum
 }
 
 pub fn day1b(lines: &Vec<String>) -> i32 {
-    let to_int = |s: &str| s.trim().parse::<i32>().unwrap();
+  let to_int = |s: &str| s.trim().parse::<i32>().unwrap();
 
-    let mut ls: Vec<i32> = vec![];
-    let mut rs = HashMap::new();
+  let mut ls: Vec<i32> = vec![];
+  let mut rs = HashMap::new();
 
-    for line in lines {
-        if let Some((l, r)) = line.split_once(' ').map(|(x, y)| (to_int(x), to_int(y))) {
-            ls.push(l);
-            rs.entry(r).and_modify(|x| *x += 1).or_insert(1);
-        }
+  for line in lines {
+    if let Some((l, r)) = line.split_once(' ').map(|(x, y)| (to_int(x), to_int(y))) {
+      ls.push(l);
+      rs.entry(r).and_modify(|x| *x += 1).or_insert(1);
     }
+  }
 
-
-    ls.iter().map(|l| {l * rs.get(&l).map_or(0, |x| *x) }).sum()
+  ls.iter().map(|l| l * rs.get(&l).map_or(0, |x| *x)).sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    const IN_SAMPLE: &str = r#"
+  const IN_SAMPLE: &str = r#"
         3   4
         4   3
         2   5
         1   3
         3   9
-        3   3
-        "#;
+        3   3"#;
 
-    const IN_FILE: &str = include_str!("data/day1.txt");
+  const IN_FILE: &str = include_str!("data/day1.txt");
 
-    fn day1_out(step: &str, i: i32) {
-        println!("{:<10}: {}", step, i);
-    }
+  fn day1_out(step: &str, i: i32) {
+    println!("{:<10}: {}", step, i);
+  }
 
-    #[test]
-    fn day1a_sample() {
-        let lines: Vec<String> = IN_SAMPLE.lines().map(|i| i.trim().to_string()).collect();
-        let out = day1a(&lines);
-        day1_out("1a sample", out);
-        assert_eq!(out, 11);
-    }
+  #[test]
+  fn day1a_sample() {
+    let lines: Vec<String> = IN_SAMPLE.lines().map(|i| i.trim().to_string()).collect();
+    let out = day1a(&lines);
+    day1_out("1a sample", out);
+    assert_eq!(out, 11);
+  }
 
-    #[test]
-    fn day1a_input() {
-        let lines: Vec<String> = IN_FILE.lines().map(|i| i.trim().to_string()).collect();
-        day1_out("1a input", day1a(&lines));
-    }
+  #[test]
+  fn day1a_input() {
+    let lines: Vec<String> = IN_FILE.lines().map(|i| i.trim().to_string()).collect();
+    day1_out("1a input", day1a(&lines));
+  }
 
-    #[test]
-    fn day1b_sample() {
-        let lines: Vec<String> = IN_SAMPLE.lines().map(|i| i.trim().to_string()).collect();
-        let out = day1b(&lines);
-        day1_out("1b sample", out);
-        assert_eq!(out, 31);
-    }
+  #[test]
+  fn day1b_sample() {
+    let lines: Vec<String> = IN_SAMPLE.lines().map(|i| i.trim().to_string()).collect();
+    let out = day1b(&lines);
+    day1_out("1b sample", out);
+    assert_eq!(out, 31);
+  }
 
-    #[test]
-    fn day1b_input() {
-        let lines: Vec<String> = IN_FILE.lines().map(|i| i.trim().to_string()).collect();
-        day1_out("1b input", day1b(&lines));
-    }
+  #[test]
+  fn day1b_input() {
+    let lines: Vec<String> = IN_FILE.lines().map(|i| i.trim().to_string()).collect();
+    day1_out("1b input", day1b(&lines));
+  }
 }
--- src/day2.rs
+++ src/day2.rs
@@ -1,86 +1,118 @@
 use std::collections::HashMap;
 use std::string;
 
-fn check(lines: &Vec<String>, is_safe: fn(&[i32]) -> bool) -> i32 {
-    let to_int = |s: &str| s.trim().parse::<i32>().unwrap();
+const MIN_STEP_SIZE: i32 = 1;
+const MAX_STEP_SIZE: i32 = 3;
 
-    lines
-        .iter()
-        .map(|s| s.trim_ascii())
-        .filter(|s| !s.is_empty())
-        .map(|l| l.split_whitespace().map(to_int).collect::<Vec<i32>>())
-        .filter(|x| is_safe(x.as_slice()))
-        .count() as i32
+#[derive(Debug, Copy, Clone)]
+enum Direction {
+  Up = 1,
+  Down = -1,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Step {
+  size: i32,
+  direction: Direction,
+  
+}
+
+impl Step {
+  fn new(i1: i32, i2: i32) -> Step {
+    let size = i2 - i1;
+    let direction = if size.is_positive() {
+      Direction::Up
+    } else {
+      Direction::Down
+    };
+    Step { size, direction }
+  }
+
+  fn is_safe_size(&self) -> bool {
+    (MIN_STEP_SIZE..=MAX_STEP_SIZE).contains(&self.size.abs())
+  }
+}
+
+fn check(lines: &Vec<String>, is_safe: fn(&[i32]) -> bool) -> i32 {
+  let to_int = |s: &str| s.trim().parse::<i32>().unwrap();
+
+  lines
+    .iter()
+    .map(|s| s.trim_ascii())
+    .filter(|s| !s.is_empty())
+    .map(|l| l.split_whitespace().map(to_int).collect::<Vec<i32>>())
+    .filter(|x| is_safe(x.as_slice()))
+    .count() as i32
 }
 
 pub fn day2a(lines: &Vec<String>) -> i32 {
-    fn is_safe(report: &[i32]) -> bool {
-        let fact = if report[0] < report[1] { 1 } else { -1 };
-        for i in 1..report.len() {
-            let step = (report[i] - report[i - 1]) * fact;
-            if step <= 0 || step > 3 {
-                return false;
-            }
-        }
-        true
+  fn is_safe(report: &[i32]) -> bool {
+    let step = Step::new(report[0], report[1]);
+    let fact = if report[0] < report[1] { 1 } else { -1 };
+    for i in 1..report.len() {
+      let (step, _) = size_and_dir(report[i - 1], report[i]);
+      if !is_step_size_safe(step) {
+        return false;
+      }
     }
+    true
+  }
 
-    check(lines, is_safe)
+  check(lines, is_safe)
 }
 
 pub fn day2b(lines: &Vec<String>) -> i32 {
-    fn is_safe(report: &[i32]) -> bool {
-        // TODO(shane): check safety for part 2b.
-        false
-    }
+  fn is_safe(report: &[i32]) -> bool {
+    // TODO(shane): check safety for part 2b.
+    false
+  }
 
-    check(lines, is_safe)
+  check(lines, is_safe)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    const IN_SAMPLE: &str = r#"
+  const IN_SAMPLE: &str = r#"
         7 6 4 2 1
         1 2 7 8 9
         9 7 6 2 1
         1 3 2 4 5
         8 6 4 4 1
-        1 3 6 7 9
-        "#;
+        1 3 6 7 9"#;
 
-    const IN_FILE: &str = include_str!("data/day2.txt");
+  const IN_FILE: &str = include_str!("data/day2.txt");
 
-    fn day2_out(step: &str, i: i32) {
-        println!("{:<10}: {}", step, i);
-    }
+  fn day2_out(step: &str, i: i32) {
+    println!("{:<10}: {}", step, i);
+  }
 
-    #[test]
-    fn day2a_sample() {
-        let lines: Vec<String> = IN_SAMPLE.lines().map(|i| i.trim().to_string()).collect();
-        let out = day2a(&lines);
-        day2_out("2a sample", out);
-        assert_eq!(out, 2);
-    }
+  #[test]
+  fn day2a_sample() {
+    let lines: Vec<String> = IN_SAMPLE.lines().map(|i| i.trim().to_string()).collect();
+    let out = day2a(&lines);
+    day2_out("2a sample", out);
+    assert_eq!(out, 2);
+  }
 
-    #[test]
-    fn day2a_input() {
-        let lines: Vec<String> = IN_FILE.lines().map(|i| i.trim().to_string()).collect();
-        day2_out("2a input", day2a(&lines));
-    }
+  #[test]
+  fn day2a_input() {
+    let lines: Vec<String> = IN_FILE.lines().map(|i| i.trim().to_string()).collect();
+    day2_out("2a input", day2a(&lines));
+  }
 
-    #[test]
-    fn day2b_sample() {
-        let lines: Vec<String> = IN_SAMPLE.lines().map(|i| i.trim().to_string()).collect();
-        let out = day2b(&lines);
-        day2_out("2b sample", out);
-        assert_eq!(out, 4);
-    }
+  #[test]
+  fn day2b_sample() {
+    let lines: Vec<String> = IN_SAMPLE.lines().map(|i| i.trim().to_string()).collect();
+    let out = day2b(&lines);
+    day2_out("2b sample", out);
+    assert_eq!(out, 4);
+  }
 
-    #[test]
-    fn day2b_input() {
-        let lines: Vec<String> = IN_FILE.lines().map(|i| i.trim().to_string()).collect();
-        day2_out("2b input", day2b(&lines));
-    }
+  #[test]
+  fn day2b_input() {
+    let lines: Vec<String> = IN_FILE.lines().map(|i| i.trim().to_string()).collect();
+    day2_out("2b input", day2b(&lines));
+  }
 }
